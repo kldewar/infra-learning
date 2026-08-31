547ade68c62e705b649977eff26346d9d7d5f091
kieran.dewar <kieran.dewar@global.com>
2026-08-31 16:49:41 +0100

    Fix argocd_apps to be a helm release so it can depend on argocd setup.

--- argocd-apps.tf
+++ argocd-apps.tf
@@ -1,38 +1,34 @@
-provider "kubernetes" {
-  host                   = kind_cluster.default.endpoint
-  client_certificate     = kind_cluster.default.client_certificate
-  client_key             = kind_cluster.default.client_key
-  cluster_ca_certificate = kind_cluster.default.cluster_ca_certificate
-}
-
-resource "kubernetes_manifest" "nginx_app" {
+resource "helm_release" "argocd_apps" {
   depends_on = [helm_release.argocd]
 
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "Application"
-    metadata = {
-      name      = "nginx"
-      namespace = "argocd"
-    }
-    spec = {
-      project = "default"
-      source = {
-        repoURL        = "https://github.com/kldewar/infra-learning-apps.git"
-        targetRevision = "main"
-        path           = "nginx"
-      }
-      destination = {
-        server    = "https://kubernetes.default.svc"
-        namespace = "nginx"
-      }
-      syncPolicy = {
-        automated = {
-          prune    = true
-          selfHeal = true
+  name             = "argocd-apps"
+  namespace        = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-apps"
+  version          = "2.0.2"
+  wait             = true
+
+  values = [yamlencode({
+    applications = {
+      nginx = {
+        project = "default"
+        source = {
+          repoURL        = "https://github.com/kldewar/infra-learning-apps.git"
+          targetRevision = "main"
+          path           = "nginx"
         }
-        syncOptions = ["CreateNamespace=true"]
+        destination = {
+          server    = "https://kubernetes.default.svc"
+          namespace = "nginx"
+        }
+        syncPolicy = {
+          automated = {
+            prune    = true
+            selfHeal = true
+          }
+          syncOptions = ["CreateNamespace=true"]
+        }
       }
     }
-  }
+  })]
 }
--- main.tf
+++ main.tf
@@ -8,10 +8,6 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.17"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.36"
-    }
   }
 
   required_version = ">= 1.0"
