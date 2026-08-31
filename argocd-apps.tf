23dadec9c63f22f9da319b9ccd43233743a42419
kieran.dewar <kieran.dewar@global.com>
2026-08-31 15:54:18 +0100

    Add nginx app deployed automatically via argocd.

---
+++ argocd-apps.tf
@@ -0,0 +1,35 @@
+provider "kubernetes" {
+  host                   = kind_cluster.default.endpoint
+  client_certificate     = kind_cluster.default.client_certificate
+  client_key             = kind_cluster.default.client_key
+  cluster_ca_certificate = kind_cluster.default.cluster_ca_certificate
+}
+
+resource "kubernetes_manifest" "nginx_app" {
+  depends_on = [helm_release.argocd]
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "nginx"
+      namespace = "argocd"
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://github.com/kldewar/infra-learning-apps.git"
+        targetRevision = "main"
+        path           = "nginx"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "nginx"
+      }
+      syncPolicy = {
+        automated = {}
+        syncOptions = ["CreateNamespace=true"]
+      }
+    }
+  }
+}
